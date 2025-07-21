e72f2b474372c2637744d7c338dbad8e9d8fad34
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2025-07-21 14:59:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ resource "random_pet" "example" {
 output "random_pet_names" {
   value = random_pet.example[*].id
 }
+
+resource "random_pet" "name" {
+  count = 100
+
+  length    = 2
+  separator = "-"
+}
+
+output "random_pet_names2" {
+  value = random_pet.name[*].id
+}
